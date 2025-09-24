fd429b3b2e58cae7262eaad05105b2b50b656cc7
CarlosRamirezSantos <carlosramii2304@gmail.com>
2025-09-24 13:00:28 +0200

    Mostrar Películas

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,28 +1,31 @@
+/**
+ * Vamos a crear dos montones de tarjetas, uno de películas y otro de recursos relacionados:
+ */
+
 const NMOVIES = 5;
 const NELEMENTSPMOVIE = 3;
 
 // Crea y mezcla el deck de películas
 const getMoviesDeck = () => {
     let movieDeck = [];
-    for (let i = 1; i <= NMOVIES; i++) {
-        // Aseguramos formato con dos dígitos (01, 02, 03...)
-        const num = String(i).padStart(2, '0');
-        movieDeck.push(num + "M");
+    for(let i = 1; i <= NMOVIES; i++) {
+        movieDeck.push("0" + i + "M");
     }
-    return _.shuffle(movieDeck);
-};
+    movieDeck = _.shuffle(movieDeck);
+    return movieDeck;
+}
 
 // Crea y mezcla el deck de elementos (recursos)
 const getElementsDeck = () => {
     let elementDeck = [];
-    for (let i = 1; i <= NMOVIES; i++) {
-        const num = String(i).padStart(2, '0');
-        for (let j = 1; j <= NELEMENTSPMOVIE; j++) {
-            elementDeck.push(num + "C" + j);
+    for(let i = 1; i <= NMOVIES; i++) {
+        for(let j = 1; j <= NELEMENTSPMOVIE; j++) {
+            elementDeck.push("0" + i + "C" + j);
         }
     }
-    return _.shuffle(elementDeck);
-};
+    elementDeck = _.shuffle(elementDeck);
+    return elementDeck;
+}
 
 // Inicializamos los decks mezclados
 let movieDeck = getMoviesDeck();
@@ -36,6 +39,7 @@ const removeMovieDeck = (deck) => {
 
 // Escuchar el evento click del botón "Mostrar película"
 document.addEventListener('DOMContentLoaded', () => {
+   
     const btnMostrarPelicula = document.getElementById('btnMostrarPelicula');
     const peliculaCaratula = document.getElementById('pelicula-caratula');
 
@@ -43,11 +47,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const pelicula = removeMovieDeck(movieDeck);
         if (pelicula) {
             // Actualiza la imagen con la película seleccionada
-            peliculaCaratula.innerHTML = `
-                <img class="elemento" src="assets/movies/${pelicula}.jpg" alt="Película">
-            `;
+            peliculaCaratula.innerHTML = `<img class="elemento" src="assets/movies/${pelicula}.jpg" alt="">`;
         } else {
             alert('No quedan más películas disponibles');
         }
     });
 });
+
